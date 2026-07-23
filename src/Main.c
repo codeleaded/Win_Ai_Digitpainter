f96e95754133b5cf633694a740818e64a50bfd2c
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:39:08 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,4 @@
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/GSprite.h"
 #include "/home/codeleaded/System/Static/Library/NeuralNetwork.h"
 
